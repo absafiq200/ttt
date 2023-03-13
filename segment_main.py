6936d13a099a4d90ccbeaaa0141701a6d7366d4b
absafiq200 <114183561+absafiq200@users.noreply.github.com>
2023-03-13 02:36:44 -0600

    Update segment_main.py

--- segment_main.py
+++ segment_main.py
@@ -10,6 +10,44 @@ logger = Logger()
 router = APIGatewayHttpRouter()
 from http_util import (response_hdr)
 
+
+@router.post("/segment/create/status")
+@tracer.capture_method   
+def getSegmentStatus():
+    error_400=''
+    error_500=''
+    try:
+        bdy = router.current_event.json_body
+        if 'file_name' not in  bdy:
+            error_400 = "segment name does not exist"                
+        else:
+            segResp = getSegmentIdsPP(bdy["pinpointprojectid"],bdy["file_name"])
+            if segResp:
+                resp = getCampSegmentStatus(bdy["pinpointprojectid"],segResp)
+                if resp:
+                    return Response(
+                        status_code = 200,
+                        body = json.dumps({
+                                "status": resp,
+                                "message" : "ok" 
+                        }),
+                        headers = response_hdr,
+                        content_type = content_types.APPLICATION_JSON 
+                    )                
+
+    except Exception as er:
+        print(er)        
+
+    return Response(
+                    status_code = 400 if error_400 else 500,
+                    body = json.dumps({
+                            "error": error_400 if error_400 else error_500 
+                    }),
+                    headers = response_hdr,
+                    content_type = content_types.APPLICATION_JSON 
+                )          
+
+
 @router.post("/segment/<type>/get")
 @tracer.capture_method   
 def getSegment(type):
@@ -51,78 +89,45 @@ def deleteSegment():
     error_500=''
     try:
         bdy = router.current_event.json_body
-        #return getCampaigns(bdy["pinpointprojectid"])
+        
         if 'pinpointprojectid' not in  bdy:
             error_400 = "pinpoint project id does not exist"                
         elif 'segment_group'  in bdy:
             segResp = getSegmentIdsPP(bdy["pinpointprojectid"],bdy["segment_group"])
             if segResp:
-                for ids in segResp:
-                    segResp = deleteSegmentPP(bdy["pinpointprojectid"],bdy["segment_id"])
-                    if segResp:
-                        error_500 = segResp
-                    
-                if not error_500:
+                err = delCampSegment(bdy["pinpointprojectid"],segResp)
+                if er:
                     return Response(
-                                status_code = 200,
-                                body = json.dumps({
-                                     'segment_group' :    bdy["segment_group"],
-                                     'segment_deletedate' : getCurrentDateTime()    
-                                }),
-                                headers = response_hdr,
-                                content_type = content_types.APPLICATION_JSON 
-                            ) 
+                        status_code = 400,
+                        body = json.dumps({
+                                "error": err 
+                        }),
+                        headers = response_hdr,
+                        content_type = content_types.APPLICATION_JSON 
+                    )
+                else:
+                    return Response(
+                                    status_code = 200,
+                                    body = json.dumps({
+                                        'segment_group' :    bdy["segment_group"],
+                                        'segment_deletedate' : getCurrentDateTime()    
+                                    }),
+                                    headers = response_hdr,
+                                    content_type = content_types.APPLICATION_JSON 
+                                ) 
             else:
                 error_400 = "Segment Does not exist"    
         elif 'segment_id' in bdy:
-            campResp = getCampaigns(bdy["pinpointprojectid"])
-            bSuccess = False
-            if campResp:
-                isInProgress = False
-                lstCampId = []
-                for cr in campResp:
-                    if cr["segment_id"] == bdy["segment_id"]: 
-                        lstCampId.append(cr["camp_id"])
-                        if cr["state"].lower() != "completed":
-                            isInProgress = True
-                            error_400 = "Campaign is in progress"
-                if not isInProgress: 
-                    if lstCampId:
-                        for iCamp in lstCampId:
-                            if not deleteCampaignpp(bdy["pinpointprojectid"],iCamp):      
-                                error_500 = "Unable to delete campaign(id):" + iCamp     
-                        if not error_500:
-                            segResp = deleteSegmentPP(bdy["pinpointprojectid"],bdy["segment_id"])
-                            if segResp:
-                                error_500 = segResp
-                    else:
-                        segResp = deleteSegmentPP(bdy["pinpointprojectid"],bdy["segment_id"])
-                        if segResp:
-                            error_500 = segResp
-    
-                            
-                                   
-
-
-
-
-
-
-                            if deleteCampaignpp(bdy["pinpointprojectid"],cr["camp_id"]):
-                                segResp = deleteSegmentPP(bdy["pinpointprojectid"],bdy["segment_id"])
-                                if segResp:
-                                    error_500 = segResp
-                                else:
-                                    bSuccess = True   
-                        else:
-                            error_500 = "Campaign is in progress"
-
-            if not bSuccess:            
-                segResp = deleteSegmentPP(bdy["pinpointprojectid"],bdy["segment_id"])
-                if segResp:
-                    error_500 = segResp
-                else:
-                    bSuccess = True    
+            err = delCampSegment(bdy["pinpointprojectid"],list[bdy["segment_id"]])
+            if er:
+                return Response(
+                    status_code = 400,
+                    body = json.dumps({
+                            "error": err 
+                    }),
+                    headers = response_hdr,
+                    content_type = content_types.APPLICATION_JSON 
+                )
             else:
                 return Response(
                                 status_code = 200,
@@ -133,9 +138,6 @@ def deleteSegment():
                                 headers = response_hdr,
                                 content_type = content_types.APPLICATION_JSON 
                             )    
-
-        
-            
 
     except Exception as er:
         print(er)        
@@ -148,3 +150,99 @@ def deleteSegment():
                     headers = response_hdr,
                     content_type = content_types.APPLICATION_JSON 
                 )          
+
+
+
+
+def delCampSegment(pinpointid,lstSegmentid):
+    error = ''
+    
+    try:
+        campResp = getCampaigns(pinpointid)
+        
+        if campResp:
+            print("campaign exist")
+            isInProgress = False
+            lstCampId = []
+            for iSeg in lstSegmentid:
+                for cr in campResp:
+                    if cr["segment_id"] == iSeg:
+                        print('campaign segment matches') 
+                        lstCampId.append(cr["camp_id"])
+                        if cr["state"]["CampaignStatus"].lower() != "completed":
+                            print('campaign is in progress')
+                            isInProgress = True
+                            error = "Campaign is in progress"
+                            break
+                    
+            if not isInProgress: 
+                if lstCampId:
+                    for iCamp in lstCampId:
+                        if not deleteCampaignpp(pinpointid,iCamp):      
+                            error = "Unable to delete campaign(id):" + iCamp     
+                    if not error:
+                        for iSeg in lstSegmentid:
+                            segResp = deleteSegmentPP(pinpointid,iSeg)
+                            if segResp:
+                                error = segResp
+                else:
+                    for iSeg in lstSegmentid:
+                        segResp = deleteSegmentPP(pinpointid,iSeg)
+                        if segResp:
+                            error = segResp         
+                                          
+        else:
+            for iSeg in lstSegmentid:
+                segResp = deleteSegmentPP(pinpointid,iSeg)
+                if segResp:
+                    error = segResp
+                
+    except Exception as er:
+        print(er)                        
+
+    return error
+
+
+def getCampSegmentStatus(pinpointid,lstSegmentid):
+    error = ''
+    
+    try:
+        campResp = getCampaigns(pinpointid)
+        
+        if campResp:
+            print("campaign exist")
+            isCompleted = False
+            isScheduled = False
+            isProcessing = False
+            isPaused = False
+            isDeleted = False
+            for iSeg in lstSegmentid:
+                for cr in campResp:
+                    if cr["segment_id"] == iSeg:
+                        if cr["state"]["CampaignStatus"].lower() == "completed":
+                            isCompleted = True
+                        elif cr["state"]["CampaignStatus"].lower() == "scheduled":
+                            isScheduled = True
+                        elif cr["state"]["CampaignStatus"].lower() == "paused":
+                            isPaused = True
+                        elif cr["state"]["CampaignStatus"].lower() == "deleted":
+                            isDeleted = True
+                        else:
+                            isProcessing = True                
+                        
+            if isProcessing:
+                return "Processing"
+            if isPaused:
+                return "Paused"
+            if isScheduled:
+                return "Scheduled"
+            if isDeleted:
+                return "Deleted"
+            if isCompleted:
+                return "Completed"                                
+                    
+                
+    except Exception as er:
+        print(er)                        
+
+    
